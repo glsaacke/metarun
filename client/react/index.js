e935c8fb2595545eee8b337c70454ae67a179100
glsaacke <143575030+glsaacke@users.noreply.github.com>
2024-08-17 16:00:29 -0500

    14: Frontend architecture & react

---
+++ client/react/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+ReactDOM.render(<Header />, document.getElementById('header-root'));
+ReactDOM.render(<Footer />, document.getElementById('footer-root'));
